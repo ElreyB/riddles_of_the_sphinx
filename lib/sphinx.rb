4d8ac7dd2355673c45fae89936833cd53d25166c
Elrey Belmonti and Rafael Furry <elrey@>
2017-09-12 09:41:45 -0700

    create #questions and pass test

--- lib/sphinx.rb
+++ lib/sphinx.rb
@@ -3,6 +3,19 @@ class Sphinx
 
   def initialize
     @wrong = 0
-    @correct = 0  
+    @correct = 0
+  end
+
+  def questions
+    questions = {
+      1 => "I do not breathe, but I run and jump. I do not eat, but I swim and stretch. I do not drink, but I sleep and stand. I do not think, but I grow and play. I do not see, but you see me every day.",
+      2 => "Always wax, yet always wane: I melt, succumbéd to the flame. Lighting darkness, with fate unblest, I soon devolve to shapeless mess.",
+      3 => "With potent, flowery words speak I, Of something common, vulgar, dry; I weave webs of pedantic prose, In effort to befuddle those, Who think I wile time away, In lofty things, above all day The common kind that linger where Monadic beings live and fare; Practical I may not be, But life, it seems, is full of me!",
+      4 => "Close to the words of life stay I, But I wither, wane, and grow dry.",
+      5 => "To you, rude would I never be, Though I flag my tongue for all to see.",
+      6 => "I welcome the day with a show of light, I steathily came here in the night. I bathe the earthy stuff at dawn, But by the noon, alas! I'm gone.",
+      7 => "Here there is no north, west, nor east, And weather fit for not man nor beast.",
+      8 => "Large as a mountain, small as a pea, Endlessly swimming in a waterless sea. "
+    }
   end
 end
--- spec/sphinx_spec.rb
+++ spec/sphinx_spec.rb
@@ -2,15 +2,26 @@ require 'rspec'
 require 'sphinx'
 
 describe 'Sphinx' do
-  let(:player) { Sphinx.new }
+  let(:sphinx) { Sphinx.new }
 
   describe '#initialize' do
      it 'will have a readable correct count' do
-       expect(player.correct).to eq 0
+       expect(sphinx.correct).to eq 0
      end
 
      it 'will have a readable wrong count' do
-       expect(player.wrong).to eq 0
+       expect(sphinx.wrong).to eq 0
      end
+  end
+
+  describe '#questions' do
+    it 'will be a Hash' do
+      expect(sphinx.questions).to be_a Hash
+    end
+
+    it 'returns a question' do
+      question = "Close to the words of life stay I, But I wither, wane, and grow dry."
+      expect(sphinx.questions[4]).to eq question
+    end
   end
 end
